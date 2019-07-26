83a19df5d1d66a50141cd814745f62bff2e94070
Frank <johnlee0722@163.com>
2019-07-26 14:36:47 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/coolweather/db/Province.java
@@ -0,0 +1,39 @@
+package com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * author: Frank
+ * created on: 2019/7/26 14:28
+ * description:
+ */
+public class Province extends DataSupport{
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
