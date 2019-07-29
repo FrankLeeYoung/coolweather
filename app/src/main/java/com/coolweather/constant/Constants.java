7555261947ecd18a89739467d2712e84386a2400
Frank <johnlee0722@163.com>
2019-07-29 16:21:48 +0800

    增加后台自动更新天气的功能。

---
+++ app/src/main/java/com/coolweather/constant/Constants.java
@@ -0,0 +1,11 @@
+package com.coolweather.constant;
+
+/**
+ * author: Frank
+ * created on: 2019/7/29 16:08
+ * description:
+ */
+public class Constants {
+
+    public static final String Key = "100e287cab4f4efea53bf80bb45e77ca";
+}
